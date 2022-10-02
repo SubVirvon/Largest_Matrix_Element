bd1c9c449bc6af34bb92a59c424d3b1fc50f8b63
Virvon <dan130205@gmail.com>
2022-10-02 15:18:32 +0300

    Largest Matrix Element

---
+++ Largest Matrix Element/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Largest_Matrix_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[10 ,10];
+            Random random = new Random();
+            int minValue = 1;
+            int maxValue = 10;
+            int maxNumber = int.MinValue;
+            int replacementNumber = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = random.Next(minValue, maxValue);
+                }
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j]);
+                    Console.Write("|");
+
+                    if (maxNumber < matrix[i, j])
+                    {
+                        maxNumber = matrix[i, j];
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == maxNumber)
+                    {
+                        matrix[i, j] = replacementNumber;
+                    }
+
+                    Console.Write(matrix[i, j]);
+                    Console.Write("|");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nНаибольшее число: " + maxNumber);
+            Console.ReadKey();
+        }
+    }
+}
